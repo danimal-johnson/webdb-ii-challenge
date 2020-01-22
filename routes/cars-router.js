054c6bc401ab5c1a3379cb23a10a03f48d40b290
Dan Johnson <danimal.johnson@gmail.com>
2020-01-22 06:52:47 -0800

    Add GET requests. Update test cases.

--- routes/cars-router.js
+++ routes/cars-router.js
@@ -3,6 +3,27 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    res.json(await db("cars").select());
+  }
+  catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.json(await db.select("*")
+              .from("cars")
+              .where("id", req.params.id)
+              .first());
+  }
+  catch (err) {
+    next(err);
+  }
+});
 
 
 
--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const knex = require('knex');
+const server = express();
 
-const knexConfiguration = {
-  client: 'sqlite3',
-  connection: {
-    filename: './data/car-dealer.db3',
-  },
-}
-const db = knex(knexConfiguration);
+// --- Middleware ---
+// server.use(helmet());
+server.use(express.json());
 
+
+// --- Routes ---
 const CarsRouter = require('./routes/cars-router.js');
+server.use('/api/cars', CarsRouter);
 // const SalesRouter = require ('./sales/sales-router.js');
+// server.use('/api/sales', SalesRouter);
 
-// const router = express.Router(); TODO: Put this in individual route files
+// --- Endpoints in this file ---
 
 server.get('/', (req, res) => {
   res.send('<h3>Car Dealership API is up and running</h3>');
 });
 
-server.get('/api/cars', req, res => {
+server.get('/api/sales', (req, res) => {
   res.status(501).end(); // TODO: Not implemented (stretch goal).
+});
+
+// --- Handle Errors ---
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong"
+  })
 })
+
+module.exports = server;
