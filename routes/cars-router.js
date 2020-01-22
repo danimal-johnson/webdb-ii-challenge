7b41783964b4cc01b03f658066ded20d9e7e69d9
Dan Johnson <danimal.johnson@gmail.com>
2020-01-22 07:08:28 -0800

    Implement remaining CRUD requests. Completed testing.

--- routes/cars-router.js
+++ routes/cars-router.js
@@ -25,6 +25,50 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    const payload = {
+      VIN: req.body.VIN,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission: req.body.transmission,
+      title_status: req.body.title_status,
+    };
+    const [id] = await db("cars").insert(payload);
+    res.json({ id });
+  }
+  catch (err) {
+    next(err);
+  }
+});
 
+router.put('/:id', async (req, res, next) => {
+  try {
+    const payload = {
+      VIN: req.body.VIN,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission: req.body.transmission,
+      title_status: req.body.title_status,
+    };
+    await db("cars").where("id", req.params.id).update(payload);
+    res.json(await db("cars").where("id", req.params.id).first());
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await db("cars").where("id", req.params.id).del();
+    res.status(204).end();
+  }
+  catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
